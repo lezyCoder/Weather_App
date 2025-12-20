965158622f4ff1b02889f90816d415cb6cf0c794
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-21 00:09:44 +0530

    Tailwind Css added &  Redux Dev tools is commented and removed

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,11 +1,11 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import { weatherReducer } from "./reducers/WeatherReducer";
-import { thunk } from "redux-thunk";
+import {thunk} from "redux-thunk";
 
-const reduxDevtools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const reduxDevtools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 export const store = createStore(
   weatherReducer,
-  compose(applyMiddleware(thunk), reduxDevtools)
+  compose(applyMiddleware(thunk))
 );
