47b5cfb31a5717e8d0cbfb0fce088ddb68a4fcb5
lezyCoder <iamvijaysingh23@gmail.com>
2025-12-25 08:56:17 +0530

    Current weather Implementation completed

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -6,9 +6,14 @@ const CurrentWeather = () => {
     (state) => state.weatherDetails
   );
 
-  const { temp } = currentWeather[0].main;
   console.log(currentWeather);
+  const temp = currentWeather[0].main.temp;
   const temperature = Math.floor(temp - 273.15);
+
+  const humidity = currentWeather[0].main.humidity;
+
+  const { speed } = currentWeather[0].wind;
+
   if (loading) return <p className="p-10">Featching Report ....</p>;
 
   return (
@@ -16,13 +21,18 @@ const CurrentWeather = () => {
       <div className="card bg-base-100 image-full w-96 shadow-sm">
         <figure>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMyjqa2bv0Ir3duJqhlKoJXp-v_7xaNfOHYw&s"
             alt="Shoes"
+            width={400}
           />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{city}</h2>
-          <p className="text-center text-xl">Temperature - {temperature} &deg;C</p>
+          <div className="weather-details flex flex-col items-end text-lg">
+            <p>Temperature - {temperature}&deg;C</p>
+            <p>Wind - {speed} mph</p>
+            <p>Humidity - {humidity} %</p>
+          </div>
           <div className="card-actions justify-end">
             {/* <button className="btn btn-primary">Buy Now</button> */}
           </div>
